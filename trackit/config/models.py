4f1eace46ed53707076a732e51a62c6e30fc35a2
aldrichmuyco <42690321+aldrichmuyco@users.noreply.github.com>
2020-08-04 10:45:00 +0800

    Migrate is_archive model field in config app

--- trackit/config/models.py
+++ trackit/config/models.py
@@ -6,6 +6,7 @@ class Department(models.Model):
     name = models.CharField(max_length=255)
     department_head = models.ForeignKey(User, related_name='dept_head', on_delete=models.CASCADE, null=True)
     is_active = models.BooleanField(default=True)
+    is_archive = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -13,6 +14,7 @@ class Department(models.Model):
 class CategoryType(models.Model):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
+    is_archive = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -21,6 +23,7 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     category_type = models.ForeignKey(CategoryType, related_name='category_types', on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
+    is_archive = models.BooleanField(default=False)
         
     def __str__(self):
         return self.name
