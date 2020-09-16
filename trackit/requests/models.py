c61ea5c16ab67c90e0a12b98326fa5b61fb7569f
aldrichmuyco <42690321+aldrichmuyco@users.noreply.github.com>
2020-09-16 14:53:22 +0800

    Migrate ticket model added category fk relation, alter reference no field

--- trackit/requests/models.py
+++ trackit/requests/models.py
@@ -2,7 +2,7 @@ import uuid
 from django.db import models
 from django_mysql.models import JSONField
 
-from config.models import Department
+from config.models import Department, Category
 from core.models import User
 
 # Create your models here.
@@ -19,9 +19,10 @@ class RequestForm(models.Model):
 # Main Service Request Model
 class Ticket(models.Model):
     ticket_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    ticket_no = models.CharField(max_length=25, blank=True)
     request_form = models.ForeignKey(RequestForm, related_name='form_type', on_delete=models.PROTECT)
     form_data = JSONField()
-    reference_no = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE, null=True)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     requested_by = models.ForeignKey(User, related_name='requestor', on_delete=models.PROTECT)
     date_created = models.DateTimeField(auto_now_add=True)
