e4c0a1124fe82851929f9f3f3f185e63a27a8c91
aldrichmuyco <42690321+aldrichmuyco@users.noreply.github.com>
2020-10-01 05:50:57 +0800

    Create functions for generating forms and categories

--- trackit/assets/src/js/request.js
+++ trackit/assets/src/js/request.js
@@ -93,7 +93,6 @@ $(document).ready(function () {
    });
 
    // LOCAL VARIABLES
-   let form_category = $('#form-group-category');
    let dd_type_id, dd_form_id;
 
    // SELECT2 CONFIGURATION
@@ -126,103 +125,18 @@ $(document).ready(function () {
       $("#formModal").modal();
       $(".modal-title").text('Make Request');
 
-      // Setting up forms 
-      form_category.hide();
+      // Hide Category Form Dropdown
+      $('#form-group-category').hide();
 
       // Get Dropdown Values
       $('#dd_types').on('change', function () {
          dd_type_id = $("#dd_types option:selected").val();
-
-         $.ajax({
-            url: '/requests/categories/json',
-            type: 'POST',
-            data: {
-               'type_id': dd_type_id
-            },
-            beforeSend: function (xhr, settings) {
-               xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            },
-            success: function (data) {
-               // Show Category Row
-               form_category.slideDown();
-
-               // Empty Dropdown Values
-               $("#dd_categories")
-                  .empty()
-                  .append('<option></option>');
-
-               // Populate Dropdown
-               data.forEach(key => {
-                  $("#dd_categories").append(`<option value='${key.id}'>${key.name}</option>`)
-               });
-            },
-         });
+         getCategory(dd_type_id);
       });
 
       $('#dd_forms').on('change', function () {
          dd_form_id = $("#dd_forms option:selected").val();
-         $(".custom-form").empty();
-
-         $.ajax({
-            url: `/api/requests/forms/${dd_form_id}`,
-            type: 'GET',
-            success: function (data) {
-               let form = JSON.parse(data.fields);
-
-               // Load form
-               form.forEach(key => {
-
-                  var name = key.title;
-                  var type = key.type;
-                  var size = key.size;
-                  var option = key.option;
-
-                  // TextBox
-                  if (type == "text" && size == "short") {
-                     $(".custom-form").append(
-                        `<div class=" form-group">
-                           <label for="nameHelp"> ${name} </label>
-                           <input type="text" class="form-control form-control-sm" id="txt_deptname"
-                              aria-describedby="nameHelp" placeholder="Enter ${name}">
-                           <small class="error-info" id="error-info-type"></small>
-                        </div>`
-                     );
-                  }
-
-                  // TextArea
-                  if (type == "text" && size == "long") {
-                     $(".custom-form").append(
-                        `<div class=" form-group">
-                           <label for="nameHelp"> ${name} </label>
-                           <textarea class="form-control form-control-sm" placeholder="Enter ${name}"
-                              rows="2"></textarea>
-                           <small class="error-info" id="error-info-type"></small>
-                        </div>`
-                     );
-                  }
-
-                  // Radio
-                  if (type == "radio") {
-                     $(".custom-form").append(
-                        `<div class=" form-group">
-                           <label for="nameHelp"> ${name}</label>
-                           <div class="form-radio"></div>
-                           <small class="error-info" id="error-info-type"></small>
-                        </div>`
-                     );
-
-                     option.forEach(data => {
-                        $(".form-radio").append(
-                           `<div class="icheck-material-orange icheck-inline m-0 mr-3">
-                              <input type="radio" id="radio_${data}" name="errortype" />
-                              <label for="radio_${data}">${data}</label>
-                           </div>`
-                        );
-                     });
-                  }
-               });
-            },
-         });
+         generateForm(dd_form_id)
       });
 
       // Prev Next Function
@@ -258,6 +172,94 @@ function navigator() {
    });
 }
 
-function generateForm() {
+function getCategory(type_id) {
+   $.ajax({
+      url: '/requests/categories/json',
+      type: 'POST',
+      data: {
+         'type_id': type_id
+      },
+      beforeSend: function (xhr, settings) {
+         xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      },
+      success: function (data) {
+         // Show Category Row
+         $('#form-group-category').slideDown();
 
+         // Empty Dropdown Values
+         $("#dd_categories")
+            .empty()
+            .append('<option></option>');
+
+         // Populate Dropdown
+         data.forEach(key => {
+            $("#dd_categories").append(`<option value='${key.id}'>${key.name}</option>`)
+         });
+      },
+   });
+}
+
+function generateForm(form_id) {
+   $(".custom-form").empty();
+
+   $.ajax({
+      url: `/api/requests/forms/${form_id}`,
+      type: 'GET',
+      success: function (data) {
+         let form = JSON.parse(data.fields);
+
+         // Load form
+         form.forEach(key => {
+
+            var name = key.title;
+            var type = key.type;
+            var size = key.size;
+            var option = key.option;
+
+            // TextBox
+            if (type == "text" && size == "short") {
+               $(".custom-form").append(
+                  `<div class=" form-group">
+                     <label for="nameHelp"> ${name} </label>
+                     <input type="text" class="form-control form-control-sm" id="txt_deptname"
+                        aria-describedby="nameHelp" placeholder="Enter ${name}">
+                     <small class="error-info" id="error-info-type"></small>
+                  </div>`
+               );
+            }
+
+            // TextArea
+            if (type == "text" && size == "long") {
+               $(".custom-form").append(
+                  `<div class=" form-group">
+                     <label for="nameHelp"> ${name} </label>
+                     <textarea class="form-control form-control-sm" placeholder="Enter ${name}"
+                        rows="2"></textarea>
+                     <small class="error-info" id="error-info-type"></small>
+                  </div>`
+               );
+            }
+
+            // Radio
+            if (type == "radio") {
+               $(".custom-form").append(
+                  `<div class=" form-group">
+                     <label for="nameHelp"> ${name}</label>
+                     <div class="form-radio"></div>
+                     <small class="error-info" id="error-info-type"></small>
+                  </div>`
+               );
+
+               option.forEach(data => {
+                  $(".form-radio").append(
+                     `<div class="icheck-material-orange icheck-inline m-0 mr-3">
+                        <input type="radio" id="radio_${data}" name="errortype" />
+                        <label for="radio_${data}">${data}</label>
+                     </div>`
+                  );
+               });
+            }
+         });
+      },
+   });
 }
