93825abd4498b869c8b6439d3aee9ebf4054dc01
Aldrich Muyco <aldrichmuyco@gmail.com>
2020-11-13 10:40:21 +0800

    Modify related_name alias in Ticket model, Add date fields in RequestForm model

--- trackit/requests/models.py
+++ trackit/requests/models.py
@@ -13,6 +13,8 @@ class RequestForm(models.Model):
     fields = JSONField()
     is_active = models.BooleanField(default=True)
     is_archive = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -21,12 +23,15 @@ class RequestForm(models.Model):
 class Ticket(models.Model):
     ticket_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     ticket_no = models.CharField(max_length=25, blank=True)
-    request_form = models.ForeignKey(RequestForm, related_name='form_type', on_delete=models.PROTECT)
+    reference_no = models.CharField(max_length=25, blank=True)
     form_data = JSONField()
-    category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE, null=True)
+    
+    request_form = models.ForeignKey(RequestForm, related_name='formtype_tickets', on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, related_name='category_tickets', on_delete=models.CASCADE, null=True)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    requested_by = models.ForeignKey(User, related_name='requestor', on_delete=models.PROTECT)
-    status = models.ForeignKey(Status, on_delete=models.SET_NULL, null=True, blank=True)
+    requested_by = models.ForeignKey(User, related_name='user_ticketes', on_delete=models.PROTECT)
+    status = models.ForeignKey(Status, related_name='status_ticketes', on_delete=models.SET_NULL, null=True, blank=True)
+
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
