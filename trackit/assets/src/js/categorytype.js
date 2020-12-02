a6a4886413f413c2584a59b93fba142d54074807
TrackIt <trackit@localhost.localdomain>
2020-12-03 00:19:50 +0800

    Set permissions on category type template

--- trackit/assets/src/js/categorytype.js
+++ trackit/assets/src/js/categorytype.js
@@ -55,8 +55,13 @@ $(document).ready(function () {
          {
             data: "null",
             render: function (data, type, row) {
-               data = `<a href='#' class='text-warning action-link btn_edit'> <i class='fas fa-pen'></i> </a>
-                     <a href='#' class='text-danger action-link btn_delete'> <i class='fas fa-trash'></i> </a>`;
+               data = '';
+               if($('#changeTypeHidden').val() == 'true') {
+                  data = data + "<a href='#' class='text-warning action-link btn_edit'> <i class='fas fa-pen'></i> </a>";
+               }
+               if($('#deleteTypeHidden').val() == 'true') {
+                  data = data + "<a href='#' class='text-danger action-link btn_delete'> <i class='fas fa-trash'></i> </a>";
+               }
                return data
             },
          }
