be5e975bb9e5a4ce5c4185ea461ed3912f4350a1
aldrichmuyco <42690321+aldrichmuyco@users.noreply.github.com>
2020-12-14 19:07:34 +0800

    Genrate form and set values for each field in update request

--- trackit/assets/src/js/request.js
+++ trackit/assets/src/js/request.js
@@ -1,8 +1,8 @@
-// Global Variables
-let fields_arr;
-
 $(document).ready(function () {
 
+   let dd_type_id, dd_form_id, dd_department_id, dd_category_id;
+   let data_obj;
+   
    // RETRIEVE / GET
    // List Table
    let table = $('#dt_requests').DataTable({
@@ -111,31 +111,200 @@ $(document).ready(function () {
       ],
    });
 
-   // // UPDATE / PUT
-   // $('#dt_requests tbody').on('click', '.btn_edit', function () {
-   //    let dt_data = table.row($(this).parents('tr')).data();
-   //    let id = dt_data['id'];
-   //    let form_data = dt_data['form_data'];
+   // SELECT2 CONFIGURATION
+   $('#dd_departments').select2({ // department select2
+      allowClear: true,
+      placeholder: 'Select Department',
+      cache: true,
+   });
 
-   //    // Assign AJAX Action Type/Method and URL
-   //    action_type = 'PUT';
-   //    url = `/api/requests/lists/${id}/`;
-   //    alert_msg = 'Update Successfully';
+   $('#dd_forms').select2({ // request form select2
+      allowClear: true,
+      placeholder: 'Select Form',
+      cache: true,
+   });
 
-   //    // Open Modal
-   //    // Rename Modal Title
-   //    $("#formModal").modal();
-   //    $(".modal-title").text('Update Requests');
+   $('#dd_types').select2({ // category type select2
+      allowClear: true,
+      placeholder: 'Select Category Type',
+      cache: true,
+   });
 
-   //    // Populate Fields
-   //    $('#dd_departments').val(dt_data['department'].id).trigger('change');
-   //    $('#dd_forms').val(dt_data['request_form'].id).trigger('change');
-   //    $('#dd_types').val(dt_data['category'].category_type.id).trigger('change');
-   //    $('#dd_categories').val(dt_data['category'].id).trigger('change');
+   $('#dd_categories').select2({ // categories select2
+      allowClear: true,
+      placeholder: 'Select Category',
+      cache: true,
+   });
 
-   //    // Prev Next Function
-   //    navigator();
-   // });
+   // SElECT ON CHANGE EVENT
+   $('#dd_departments').on('change', function () { // department dropdown
+      dd_department_id = $("#dd_departments option:selected").val();
+   });
+
+   $('#dd_forms').on('change', function () { // request form dropdown
+      dd_form_id = $("#dd_forms option:selected").val();
+
+      axios({
+         method: 'GET',
+         url: `/api/requests/forms/${dd_form_id}`,
+         headers: axiosConfig,
+      }).then(function (response) { // clear div elements
+         $('.body-info').remove();
+         $('.form-wrapper').empty();
+         $(".form-admin-wrapper").empty();
+
+         return response.data.fields
+      }).then(function (response) { // Add to global object
+         return data_obj = new Object(response)
+      }).then(function (response) { // generate form fields
+         response.forEach(data => {
+            const title = data.title;
+            const form_field = data.form_field;
+            const is_admin = data.is_admin;
+            let form_wrapper;
+
+            if (is_admin == true) {
+               form_wrapper = $(".form-admin-wrapper")
+               $('.separator').removeClass('d-none');
+            } else {
+               form_wrapper = $(".form-wrapper")
+            }
+
+            if (form_field.length > 1) {
+               form_wrapper.append(
+                  `<div class="form-group">
+                        <label> ${title} </label>
+                        <div class="type-group"></div>
+                        <small class="error-info" id="error-info-type"></small>
+                     </div>`
+               );
+
+               form_field.forEach(field => {
+                  if (field.type == "text") {
+                     $(".type-group").append(
+                        `<input type="text" class="form-control form-control-sm" id="${field.id}" placeholder="Enter ${title}">`
+                     );
+                  }
+                  if (field.type == "textarea") {
+                     $(".type-group").append(
+                        `<textarea class="form-control form-control-sm" id="${field.id}" placeholder="Enter ${title}" rows="2"></textarea>`
+                     );
+                  }
+                  if (field.type == "radio") {
+                     field.option.forEach(opt => {
+                        $(".type-group").append(
+                           `<div class="icheck-material-orange icheck-inline m-0 mr-3">
+                           <input type="radio" id="${opt.id}" name="${field.id}" />
+                           <label for="${opt.id}">${opt.name}</label>
+                        </div>`
+                        );
+                     });
+                  }
+                  if (field.type == "check") {
+                     counter = 1;
+                     field.option.forEach(opt => {
+                        $(".type-group").append(
+                           `<div class="icheck-material-orange icheck-inline m-0 mr-3">
+                           <input type="checkbox" id="${opt.id}"/>
+                           <label for="${opt.id}">${opt.name}</label>
+                        </div>`
+                        );
+                        counter++;
+                     });
+                  }
+               });
+
+            } else {
+               if (form_field.type == "text") {
+                  form_wrapper.append(
+                     `<div class=" form-group">
+                           <label> ${title} </label>
+                           <input type="text" class="form-control form-control-sm" id="${form_field.id}" placeholder="Enter ${title}">
+                           <small class="error-info" id="error-info-type"></small>
+                        </div>`
+                  );
+               }
+               if (form_field.type == "textarea") {
+                  form_wrapper.append(
+                     `<div class=" form-group">
+                           <label> ${title} </label>
+                           <textarea class="form-control form-control-sm" id="${form_field.id}" placeholder="Enter ${title}" rows="2"></textarea>
+                           <small class="error-info" id="error-info-type"></small>
+                        </div>`
+                  );
+               }
+               if (form_field.type == "radio") {
+                  form_wrapper.append(
+                     `<div class=" form-group">
+                           <label> ${title} </label>
+                           <div class="radio-group"></div>
+                           <small class="error-info" id="error-info-type"></small>
+                        </div>`
+                  );
+                  form_field.option.forEach(opt => {
+                     $(".radio-group").append(
+                        `<div class="icheck-material-orange icheck-inline m-0 mr-3">
+                              <input type="radio" id="${opt.id}" name="${form_field.id}" />
+                              <label for="${opt.id}">${opt.name}</label>
+                           </div>`
+                     );
+                  });
+               }
+               if (form_field.type == "check") {
+                  form_wrapper.append(
+                     `<div class=" form-group">
+                           <label> ${title}</label>
+                           <div class="check-group"></div>
+                           <small class="error-info" id="error-info-type"></small>
+                        </div>`
+                  );
+
+                  form_field.option.forEach(opt => {
+                     $(".check-group").append(
+                        `<div class="icheck-material-orange icheck-inline m-0 mr-3">
+                              <input type="checkbox" id="${opt.id}"/>
+                              <label for="${opt.id}">${opt.name}</label>
+                           </div>`
+                     );
+                  });
+               }
+            }
+         });
+
+      }).then(function (response) { // collapse div
+         $('#collapseTwo').removeAttr('data-parent').collapse()
+      });
+
+   });
+
+   $('#dd_types').on('change', function () { // category type dropdown
+      dd_type_id = $("#dd_types option:selected").val();
+
+      axios({
+         method: 'POST',
+         url: '/requests/categories/json',
+         data: {
+            'type_id': dd_type_id
+         },
+         headers: axiosConfig,
+      }).then(function (response) { // set dropdown status
+   
+         $("#dd_categories")
+            .empty()
+            .append('<option></option>')
+            .removeAttr('disabled');
+   
+         return response.data
+      }).then(function (response) { // set dropdown status
+         response.forEach(key => {
+            $("#dd_categories").append(`<option value='${key.id}'>${key.name}</option>`)
+         });
+      });
+   });
+
+   $('#dd_categories').on('change', function () { // categories dropdown
+      dd_category_id = $("#dd_categories option:selected").val();
+   });
 
    // Submit Form
    $("#btn_save").click(function (e) {
@@ -148,7 +317,7 @@ $(document).ready(function () {
       // Data
       data.ticket_no = '';
       data.request_form = dd_form_id;
-      data.form_data = getFormValues(fields_arr);
+      data.form_data = getFormValues(data_obj);
       data.category = dd_category_id;
       data.department = dd_department_id;
       data.is_active = true;
@@ -156,18 +325,23 @@ $(document).ready(function () {
 
       if (success == 1) {
          axios({
-            method: action_type,
-            url: url,
+            method: 'POST',
+            url: '/api/requests/lists/',
             data: data,
             headers: axiosConfig,
          }).then(function (response) { // success
-            Toast.fire({
-               icon: 'success',
-               title: alert_msg,
+            // disable submit button
+            $(this).attr('disabled', true)
+            $.when(
+               Toast.fire({
+                  icon: 'success',
+                  title: 'Submit Successfully',
+               }),
+               $('.overlay').removeClass('d-none')
+            ).then(function () {
+               $(location).attr('href', '/requests/lists')
             });
-            $('#formModal').modal('toggle');
-            $("#form").trigger("reset");
-            table.ajax.reload();
+
          }).catch(function (error) { // error
             console.log(error.response)
 
@@ -179,3 +353,92 @@ $(document).ready(function () {
       };
    });
 });
+
+// Get form Values
+function getFormValues(data_obj) {
+   let form_fields_obj = new Array();
+
+   data_obj.forEach(data => {
+      const title = data.title;
+      const form_field = data.form_field;
+      const is_admin = data.is_admin;
+      let answer, fields;
+
+      if (form_field.length > 1) {
+         fields = new Array();
+
+         form_field.forEach(field => {
+            if (field.type == "text" || field.type == "textarea") { // textfield
+               answer = $(`#${field.id}`).val();
+            }
+            if (field.type == "radio") { // radio button
+               field.option.forEach(opt => {
+                  if ($(`#${opt.id}`).is(":checked")) {
+                     answer = new Object();
+                     answer.key = opt.id;
+                     answer.value = true;
+                  }
+               });
+            }
+            if (field.type == "check") { // checkbox
+               answer = new Array();
+               field.option.forEach(opt => {
+                  if ($(`#${opt.id}`).is(":checked")) {
+                     answer.push({
+                        "key": opt.id,
+                        "value": true
+                     })
+                  }
+               });
+            }
+
+            fields.push({
+               "id": field.id,
+               "name": field.name,
+               "type": field.type,
+               "option": field.option,
+               "answer": answer,
+            });
+         });
+      } else {
+         if (form_field.type == "text" || form_field.type == "textarea") { // texfield
+            answer = $(`#${form_field.id}`).val();
+         }
+         if (form_field.type == "radio") { // radio button
+            form_field.option.forEach(opt => {
+               if ($(`#${opt.id}`).is(":checked")) {
+                  answer = new Object();
+                  answer.key = opt.id;
+                  answer.value = true;
+               }
+            });
+         }
+         if (form_field.type == "check") { // checkbox
+            answer = new Array();
+            form_field.option.forEach(opt => {
+               if ($(`#${opt.id}`).is(":checked")) {
+                  answer.push({
+                     "key": opt.id,
+                     "value": true
+                  })
+               }
+            });
+         }
+
+         fields = new Object();
+         fields.id = form_field.id;
+         fields.name = form_field.name;
+         fields.type = form_field.type;
+         fields.option = form_field.option;
+         fields.answer = answer;
+      }
+
+      form_fields_obj.push({
+         "title": title,
+         "form_field": fields,
+         "is_admin": is_admin,
+      });
+   });
+
+   return form_fields_obj;
+}
--- trackit/requests/views.py
+++ trackit/requests/views.py
@@ -26,7 +26,13 @@ def create_ticket(request):
 def detail_ticket(request, ticket_id):
    tickets = get_object_or_404(Ticket, ticket_id=ticket_id)
    
-   return render(request, 'pages/requests/ticket_detail.html', {'ticket': tickets})
+   forms= RequestForm.objects.filter(is_active=True, is_archive=False)
+   types =  CategoryType.objects.filter(is_active=True, is_archive=False)
+   departments =  Department.objects.filter(is_active=True, is_archive=False)
+   categories = Category.objects.filter(category_type=tickets.category.category_type, is_active=True, is_archive=False)
+   
+   context = {'tickets': tickets, 'forms': forms, 'types': types, 'departments':departments, 'categories':categories}
+   return render(request, 'pages/requests/ticket_detail.html', context)
 
 @login_required
 def boards(request):
