a16239fc945bc1c4cd01ae762588d14cb10dfde8
kaypee <kaypee.casensa@gmail.com>
2020-12-16 13:42:03 +0800

    Add department (ManyToManyField) to User model.

--- trackit/core/models.py
+++ trackit/core/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-   pass
+   department = models.ManyToManyField('config.Department', through='UserDepartment')
 
    def __str__(self):
       return '%s %s' % (self.first_name, self.last_name)
 
-# Create your models here.
+class UserDepartment(models.Model):
+   user = models.ForeignKey(User, on_delete=models.CASCADE)
+   department = models.ForeignKey('config.Department', on_delete=models.CASCADE)
+
+   def __str__(self):
+      return '%s (%s)' % (self.user, self.department)
