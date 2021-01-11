21cdb3eda5d5b353e6edf0100ab43ea4ae02f09e
Aldrich Muyco <aldrichmuyco@gmail.com>
2021-01-11 13:14:52 +0800

    Replace drag and drop API to jquery UI sortable

--- trackit/assets/src/js/boards.js
+++ trackit/assets/src/js/boards.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
 
     $('#select_form').on('change', function() {
         let form_id = $($(this), "option:selected").val();
-        let draggedItem = null;
 
         axios.get(`/api/requests/forms/${form_id}/`, {headers : axiosConfig}).then(response => {
             const form_status = response.data.status;
@@ -19,8 +18,7 @@ $(document).ready(function () {
                     `<div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
                         <div class="card card-boards bg-secondary mb-2">
                             <div class="card-header"> <div class="card-title">${status.name}</div> </div>
-                            <div class="card-body card-body-${status.id} list"></div>
-                            <div class="card-footer"></div>
+                            <div class="card-body card-body-${status.id} card-body-column"></div>
                         </div>
                     </div>`
                 );
@@ -38,7 +36,7 @@ $(document).ready(function () {
                         let name_repr =  fname_char.concat(lname_char)
 
                         card_body.append(
-                            `<div class="list-item" draggable="true">
+                            `<div class="list-item" data-ticket-id="${tic.ticket_id}">
                                 <div class="list-item-body">
                                     <p><b>#${tic.ticket_no}</b> - ${tic.category.name} </p>  
                                 </div>
@@ -49,49 +47,21 @@ $(document).ready(function () {
                         ); 
                     });
                 }).then(response => {
-                    // Draggables
-                    const list_items = $('.list-item');
-                    const lists = $('.list');
+                    $('.card-body-column').sortable({
+                        connectWith: ".card-body-column",
+                        start: function (event, ui) {
+                            ui.item.addClass('tilt')
+                            ui.placeholder.height(ui.item.height());
+                            ui.placeholder.css('visibility', 'visible');
+                            ui.placeholder.css('background-color', '#d9dce6');
+                            ui.placeholder.css('box-shadow', 'none');
+                        },
+                        stop: function (event, ui) {
+                            ui.item.removeClass('tilt');
+                        },
+                    });
 
-                    list_items.each(function (index, element) {
-                        const item = list_items[index];
-
-                        $(item).on('dragstart', function () {
-                            draggedItem = item;
-                            setTimeout(function () { 
-                                item.style.display = 'none';
-                            }, 0)
-                        });
-
-                        $(item).on('dragend', function () {
-                            setTimeout(function () {
-                                draggedItem.style.display = 'block';
-                                draggedItem = null;
-                            }, 0);
-                        });
-                    })
-
-                    lists.each(function (index, element) {
-                        const list = lists[index];
-
-                        $(list).on('dragover', function (e) {
-                            e.preventDefault();
-                        });
-
-                        $(list).on('dragenter', function (e) {
-                            e.preventDefault();
-                            this.style.backgroundColor = '#d8d9e1';
-                        });
-
-                        $(list).on('dragleave', function (e) {
-                            this.style.backgroundColor = '#e3E5ED';
-                        });
-
-                        $(list).on('drop', function (e) {
-                            this.append(draggedItem);
-                            this.style.backgroundColor = '#e3E5ED';
-                        });
-                    })
+                    $('.card-body-column').disableSelection();
                 });
             });            
         });
