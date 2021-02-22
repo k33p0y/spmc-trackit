07305504c899ecd799c2607b681b21567bc91be2
Aldrich Muyco <aldrichmuyco@gmail.com>
2021-02-22 14:39:11 +0800

    Add remark defualt value, Filter action viewing dependent to step status

--- trackit/requests/urls.py
+++ trackit/requests/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
    path('requests/new', views.create_ticket, name='create_ticket'),
    path('requests/<uuid:ticket_id>/detail', views.detail_ticket, name='detail_ticket'),
    path('requests/<uuid:ticket_id>/view', views.view_ticket, name='view_ticket'),
-   path('requests/boards', views.boards, name='boards'),
    path('requests/track', views.ticket_log_list, name='ticket_log_list'),
    path('api/ticket/logs/', CRUDEventList.as_view()), # easyaudit_crudevent api
    path('api/requests/comments/', CommentListCreateAPIView.as_view(), name='list-create-comment'), # list/create comment api
--- trackit/requests/views.py
+++ trackit/requests/views.py
@@ -15,15 +15,18 @@ def ticket(request):
    departments =  Department.objects.filter(is_active=True, is_archive=False)
    types = CategoryType.objects.filter(is_active=True, is_archive=False)
    statuses = Status.objects.filter(is_active=True, is_archive=False)
-   return render(request, 'pages/requests/ticket_lists.html', {'tickets': tickets, 'departments':departments, 'types':types, 'statuses': statuses})
+
+   context = {'tickets': tickets, 'departments':departments, 'types':types, 'statuses': statuses}
+   return render(request, 'pages/requests/ticket_lists.html', context)
 
 @login_required
 def create_ticket(request):
    forms= RequestForm.objects.filter(is_active=True, is_archive=False)
    types =  CategoryType.objects.filter(is_active=True, is_archive=False)
    departments =  Department.objects.filter(is_active=True, is_archive=False)
+
    context = {'forms': forms, 'types': types, 'departments':departments}
-   return render(request, 'pages/requests/ticket_new.html', context)
+   return render(request, 'pages/requests/ticket_new.html', context)   # if request.user has_perm('requests_change_')   # if request.user has_perm('requests_change_')
 
 @login_required
 def detail_ticket(request, ticket_id):
@@ -34,27 +37,41 @@ def detail_ticket(request, ticket_id):
    departments =  Department.objects.filter(is_active=True, is_archive=False)
    categories = Category.objects.filter(category_type=ticket.category.category_type, is_active=True, is_archive=False)
 
-   formstatuses = RequestFormStatus.objects.select_related('form', 'status').filter(form_id=ticket.request_form).order_by('order')   
+   steps = RequestFormStatus.objects.select_related('form', 'status').filter(form_id=ticket.request_form).order_by('order')   
    attachments = Attachment.objects.filter(ticket_id=ticket_id).order_by('-uploaded_at')
 
-   for formstatus in formstatuses:
-      if(formstatus.status == ticket.status):
-         step = formstatuses.get(status_id=ticket.status)
-         
-   last_step = formstatuses.latest('order')
+   remark = None
 
-   context = {'ticket': ticket, 'forms': forms, 'types': types, 'departments':departments, 'categories':categories, 'attachments':attachments, 'steps':formstatuses, 'step':step, 'last_step':last_step}
+   for step in steps:
+      if(step.status == ticket.status):
+         curr_step = steps.get(status_id=ticket.status)
+
+      remarks = ticket.remarks.filter(ticket_id=ticket_id, status_id=step.status_id, is_approve=True) 
+      if step.is_client_step and step.has_approving: 
+         remark = remarks.earliest('id') if remarks else None
+         
+   context = {
+      'ticket': ticket, 
+      'forms': forms, 
+      'types': types, 
+      'departments':departments, 
+      'categories':categories, 
+      'attachments':attachments, 
+      'steps':steps, 
+      'curr_step':curr_step, 
+      'last_step':steps.latest('order'),
+      'remark' : remark
+   }
    return render(request, 'pages/requests/ticket_detail.html', context)
 
 
 @login_required
 def view_ticket(request, ticket_id):
    ticket = get_object_or_404(Ticket, ticket_id=ticket_id)
-
    steps = RequestFormStatus.objects.select_related('form', 'status').filter(form_id=ticket.request_form).order_by('order')   
    attachments = Attachment.objects.filter(ticket_id=ticket_id).order_by('-uploaded_at')
 
-   last_step = steps.latest('order')
+   remark = None
 
    for step in steps:
       if step.status == ticket.status: 
@@ -64,15 +81,15 @@ def view_ticket(request, ticket_id):
       if step.is_client_step and step.has_approving: 
          remark = remarks.earliest('id') if remarks else None
 
-   context = {'ticket': ticket, 'attachments':attachments, 'steps':steps, 'curr_step':curr_step, 'last_step':last_step, 'remark': remark}
+   context = {
+      'ticket': ticket, 
+      'attachments':attachments, 
+      'steps':steps, 
+      'curr_step':curr_step, 
+      'last_step':steps.latest('order'), 
+      'remark': remark
+   }
    return render(request, 'pages/requests/ticket_view.html', context)
-
-@login_required
-def boards(request):
-   forms= RequestForm.objects.filter(is_active=True, is_archive=False)
-   
-   context = {'forms':forms}
-   return render(request, 'pages/requests/boards.html', context)
-
+      
 def ticket_log_list(request):
    return render(request, 'pages/requests/track.html', {})
