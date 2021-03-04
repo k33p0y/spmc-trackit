d93c805192d87f84c1198b37db091f738a2d9c28
Aldrich Muyco <aldrichmuyco@gmail.com>
2021-03-04 14:55:37 +0800

    Allow null to department FK in user model

--- trackit/core/models.py
+++ trackit/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-   department = models.ForeignKey('config.Department', on_delete=models.CASCADE)
+   department = models.ForeignKey('config.Department', null=True, on_delete=models.CASCADE)
    middle_name = models.CharField(max_length=50, blank=True)
    suffix = models.CharField(max_length=5, blank=True)
 
