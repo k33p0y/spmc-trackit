ef7edd1532b354ee585a5301b6484a20729e61b4
Aldrich Muyco <aldrichmuyco@gmail.com>
2021-12-02 11:02:10 +0800

    Move notice modal to register template. alter field arrangement

--- trackit/assets/src/js/auth/login.js
+++ trackit/assets/src/js/auth/login.js
@@ -1,29 +1,3 @@
 $(document).ready(function () {
-    // show notice modal
-    $('#btn_register').click(function () {
-        // reset form in modal
-        $('#chk_agree').prop('checked', false);
-        $('#btn_agree').prop('disabled', true);
 
-        // modal option
-        $('#noticeModal').modal({
-            show: true,
-            backdrop: 'static',
-            keyboard: false,
-        });
-    });
-
-    // notice modal event
-    $('#noticeModal').on('shown.bs.modal', function (e) {
-        // Checkbox change event enable button
-        $('#chk_agree').change(function () {
-            if (this.checked) $('#btn_agree').prop('disabled', false);
-            else $('#btn_agree').prop('disabled', true);
-        });
-        // Aggree button
-        $('#btn_agree').click(function () {
-            $('#noticeModal').modal('hide');
-            $(location).attr('href', '/registration');
-        });
-    })
 })
--- trackit/assets/src/js/auth/register.js
+++ trackit/assets/src/js/auth/register.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
+    // data privacy notice modal
+    // reset form in modal
+    $('#chk_agree').prop('checked', false);
+    $('#btn_agree').prop('disabled', true);
+    // modal option
+    $('#noticeModal').modal({
+        show: true,
+        backdrop: 'static',
+        keyboard: false,
+    });
+    // notice modal event
+    $('#noticeModal').on('shown.bs.modal', function (e) {
+        // Checkbox change event enable button
+        $('#chk_agree').change(function () {
+            if (this.checked) $('#btn_agree').prop('disabled', false);
+            else $('#btn_agree').prop('disabled', true);
+        });
+        // Aggree button
+        $('#btn_agree').click(function () {
+            $('#noticeModal').modal('hide');
+        });
+    })
+
     // select2 config
     $('#select2_department').select2({
         placeholder: 'Department',
@@ -10,34 +33,29 @@ $(document).ready(function () {
         createUsername();
     });
 
-    // prevent field to enter letters
-    $("#txt_contact").keyup(function () {
-        $("#txt_contact").val(this.value.match(/[0-9]*/));
-    });
-
     // submit sign up form
     $("#btn_signup").click(function (e) {
         e.preventDefault();
 
-        // Variables
-        let data = {}
-        data.firstname = $('#txt_firstname').val();
-        data.lastname = $('#txt_lastname').val();
-        data.contact = $('#txt_contact').val();
-        data.license = $('#txt_license').val();
+        // declare new object
+        let data = new Object();
+        data.first_name = $('#txt_firstname').val();
+        data.last_name = $('#txt_lastname').val();
+        data.contact_no = $('#txt_contact').val();
+        data.license_no = $('#txt_license').val();
         data.department = $('#select2_department').val();
         data.email = $('#txt_email').val();
         data.username = $('#txt_username').val();
-        data.password1 = $('#txt_password1').val();
+        data.password = $('#txt_password1').val();
         data.password2 = $('#txt_password2').val();
 
     }); // submit form end
 
+    // concat firstname and lastname for username
     let createUsername = function () {
         let first_name = $("#txt_firstname").val().charAt(0)
         let last_name = $("#txt_lastname").val();
         let username = first_name + last_name
         $('#txt_username').val(username.toLowerCase())
     }
-
 })
