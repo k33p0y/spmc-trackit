fa980de11dda37d1fc4c497aa6bbfe09cd242924
Aldrich Muyco <aldrichmuyco@gmail.com>
2021-12-14 09:37:19 +0800

    Show and hide grids in user profile for verified and not verified ueser

--- trackit/assets/lib/js/websocket-comment.js
+++ trackit/assets/lib/js/websocket-comment.js
@@ -1,26 +1,26 @@
 let url_ws_comment = wsStart + loc.host + '/ws/comments/';
 const socket_comment = new WebSocket(url_ws_comment)
 
-socket_comment.onopen = function(event) {
+socket_comment.onopen = function (event) {
 }
 
-socket_comment.onmessage = function(event) {
+socket_comment.onmessage = function (event) {
     displayComment(JSON.parse(event.data));
 }
 
-socket_comment.onclose = function(event) {
-    $('.online-status').removeClass('text-success');
+socket_comment.onclose = function (event) {
+    $('.online-status, .profile-online-status').addClass('d-none');
 }
 
-socket_comment.onerror = function(event) {
+socket_comment.onerror = function (event) {
     console.log(event)
     console.log('something went wrodddng')
 }
 
-let displayComment = function(obj){
+let displayComment = function (obj) {
     if ($('#btn-post-comment').data().ticketId === obj.comment['ticket_id']) {
         let logged_user_id = actor
-        
+
         $('.comment-section').prepend(
             `<div class="user-comment justify-content-start ${logged_user_id == obj.comment['user_id'] ? 'bg-comment-orange' : ''}">
                 <div class="d-inline justify-content-start ">
--- trackit/assets/lib/js/websocket-notification.js
+++ trackit/assets/lib/js/websocket-notification.js
@@ -1,33 +1,33 @@
 let url_ws_notification = wsStart + loc.host + '/ws/notifications/';
 const socket_notification = new WebSocket(url_ws_notification)
 
-socket_notification.onopen = function(event) {
-    $('.online-status').addClass('text-success')
+socket_notification.onopen = function (event) {
+    $('.online-status, .profile-online-status').removeClass('d-none')
     getAllNotifications();
 }
 
-socket_notification.onmessage = function(event) {
+socket_notification.onmessage = function (event) {
     //console.log(event)
-    
+
     if ('notification' in JSON.parse(event.data)) getAllNotifications(); // Notifications
 }
 
-socket_notification.onclose = function(event) {
-    $('.online-status').removeClass('text-success');
+socket_notification.onclose = function (event) {
+    $('.online-status, .profile-online-status').addClass('d-none');
 }
 
-socket_notification.onerror = function(event) {
+socket_notification.onerror = function (event) {
     console.log(event)
     console.log('something went wrong')
 }
 
-let getAllNotifications = function(){
-    getUserNotifications().then(data=>{
+let getAllNotifications = function () {
+    getUserNotifications().then(data => {
         let notifications = data
-        unread_count = notifications.filter(obj=>obj.unread==true).length
-        
+        unread_count = notifications.filter(obj => obj.unread == true).length
 
-        if (notifications.length){ // if notification is available
+
+        if (notifications.length) { // if notification is available
             if (unread_count > 0) {
                 $('.new-notifications-count').html(unread_count); // display unread notification to bell
                 $('#clear-all-notifications').removeClass('d-none')
@@ -35,12 +35,12 @@ let getAllNotifications = function(){
 
             // var saveDropdown = $('.dropdown-notifications-div .dropdown-body').detach();
             $('.dropdown-notifications-div .dropdown-body').empty();
-            for (i=0; i<notifications.length; i++){
+            for (i = 0; i < notifications.length; i++) {
                 let model = JSON.parse(notifications[i].log.object_json_repr)
                 if (JSON.parse(model[0].model == 'requests.ticket')) displayTicketNotification(notifications[i]);
                 if (JSON.parse(model[0].model == 'requests.comment')) displayCommentNotification(notifications[i]);
             }
-            $('.dropdown-notifications-div .dropdown-body a').click(function(){
+            $('.dropdown-notifications-div .dropdown-body a').click(function () {
                 localStorage.setItem("ticketNumber", $(this).attr('data-ticket-no'));
                 localStorage.setItem("notification-id", $(this).attr('data-notification-id'));
             })
@@ -53,7 +53,7 @@ let getAllNotifications = function(){
     });
 };
 
-let displayTicketNotification = function(notification){
+let displayTicketNotification = function (notification) {
     let object_json_repr = JSON.parse(notification.log.object_json_repr)
     let ticket_no = object_json_repr[0].fields.ticket_no
     let description = object_json_repr[0].fields.description
@@ -64,7 +64,7 @@ let displayTicketNotification = function(notification){
     time_from_now = moment(notification.log.datetime).fromNow()
     if (action === 'Create') action = `Created a new request: <b class="text-orange">${ticket_no}</b>.`;
     else if (action === 'Update') {
-        if (notification.log.remarks){
+        if (notification.log.remarks) {
             if (notification.log.remarks.is_approve) action = `Approved request: <b class="text-orange">${ticket_no}</b> changed status to <b class="text-orange">${notification.log.changed_fields.status[1]}</b>.`
             else if (notification.log.remarks.is_approve == false) action = `Disapproved request: <b class="text-orange">${ticket_no}</b> changed status to <b class="text-orange">${notification.log.changed_fields.status[1]}</b>.`
             else action = `Updated request: <b class="text-orange">${ticket_no}</b> status to <b class="text-orange">${notification.log.changed_fields.status[1]}</b>.`
@@ -82,19 +82,19 @@ let displayTicketNotification = function(notification){
                 <p class="text-muted line-clamp notification-overview mt-1"><i class="fas fa-ticket-alt text-info mr-1"></i><i class="text-muted">${description}</i></p>
                 <span class="text-black-50 notification-time">${time_from_now}</span>    
             </div>
-        </a>`   
+        </a>`
     )
 };
 
-let displayCommentNotification = function(notification){
+let displayCommentNotification = function (notification) {
     created_by = `${notification.log.user.first_name} ${notification.log.user.last_name}`
     user = `${notification.user.first_name} ${notification.user.last_name}`
 
     if (created_by === user) self = 'You'; // if the authenticated user is the requestor of the ticket
     let object_json_repr = JSON.parse(notification.log.object_json_repr)
-    
+
     time_from_now = moment(notification.log.datetime).fromNow();
-    
+
     let ticket_id = `${object_json_repr[0].fields.ticket}`
     let ticket_no = notification.log.ticket_no
     let comment = `${object_json_repr[0].fields.content}`
@@ -110,18 +110,18 @@ let displayCommentNotification = function(notification){
                 <p class="text-muted line-clamp notification-overview mt-1"><i class="fas fa-comment text-success mr-1"></i><i class="text-muted">${comment}</i></p>
                 <span class="text-black-50 notification-time">${time_from_now}</span>  
             </div>
-        </a>`   
+        </a>`
     )
 };
 
 // Clear all notifications
-$('#clear-all-notifications').click(function(){
+$('#clear-all-notifications').click(function () {
     let notifications = $('.dropdown-body').children();
 
-    $.each(notifications, function(index, value) {
+    $.each(notifications, function (index, value) {
         const notification_id = $(this).data().notificationId;
-        axios.delete(`/api/user/notifications/${notification_id}/`, {headers: axiosConfig}).then(res => {
-            $(this).fadeOut("fast", "linear", function() {$(this).remove();});
+        axios.delete(`/api/user/notifications/${notification_id}/`, { headers: axiosConfig }).then(res => {
+            $(this).fadeOut("fast", "linear", function () { $(this).remove(); });
         })
         getAllNotifications();
     });
