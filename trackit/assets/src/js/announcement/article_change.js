23c36a01273152f136ece396b7eabe518d04980a
Aldrich Muyco <aldrichmuyco@gmail.com>
2022-02-16 14:42:56 +0800

    Modify templates for responisve

--- trackit/assets/src/js/announcement/article_change.js
+++ trackit/assets/src/js/announcement/article_change.js
@@ -1,44 +1,31 @@
 $(document).ready(function () {
-    
-    // save and publish button
-    $('#btn_publish').click(function (e) {
-        e.preventDefault();
-        const article = $(this).data().articleId;
-        let data = new Object();
-        data.title = $('#txt_title').val();
-        data.preface = $('#txt_preface').val();
-        data.content = 'Sample Content';
-        data.is_publish = true;
-        data.is_active = ($('#is_active_switch').is(":checked")) ? true : false;
-        putArticle(data, article)
-    });
-
     // save button 
     $('#btn_save').click(function (e) {
         e.preventDefault();
+        $(this).attr('disabled', true) //  disable button
         const article = $(this).data().articleId;
         let data = new Object();
         data.title = $('#txt_title').val();
         data.preface = $('#txt_preface').val();
         data.content = 'Sample Content';
+        data.is_publish = ($('#is_publish_switch').is(":checked")) ? true : false; 
         data.is_active = ($('#is_active_switch').is(":checked")) ? true : false; 
-        putArticle(data, article)
-    });
-
-    let putArticle = function(data, article) {
+        
         axios({
             method: 'PUT',
             url: `/api/announcement/all/article/${article}/`,
             data: data,
             headers: axiosConfig,
         }).then(function (res) { // success
-            $.when(toastSuccess('Success')).then(() => $(location).attr('href', '/announcement/lists')) // Alert
+            $.when(toastSuccess('Success')).then(() => {
+                $('#btn_save').attr('disabled', false) // enable button
+                $(location).attr('href', '/announcement/lists')
+            }) // Alert
         }).catch(function (err) { // error
             toastError(err.response.statusText) // alert
             if (err.response.data.title) showFieldErrors(err.response.data.title, 'title'); else removeFieldErrors('title');
         });
-    };
-
+    });
 
     let showFieldErrors = function(obj, field) {
         // Enable button
