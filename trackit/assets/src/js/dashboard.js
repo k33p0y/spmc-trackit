8d5824e4a5d25d79e1917f7f19a7bcc223b2aa88
Aldrich Muyco <aldrichmuyco@gmail.com>
2022-02-22 14:57:18 +0800

    Add function for READ MORE button in carousel. Modal template

--- trackit/assets/src/js/dashboard.js
+++ trackit/assets/src/js/dashboard.js
@@ -11,4 +11,25 @@ $(document).ready(function () {
          "orderable": false
       }]
    });
+
+   $('.carousel-inner').on('click', '.btn-more', function() {
+      axios
+         .get(`/api/announcement/all/article/${$(this).data().articleId}/`)
+         .then(res => {
+            console.log(res.data)
+            $('.modal-readmore').html(`
+               <div class="m-2">
+                  <h6 class="font-weight-bold text-orange mb-1">${res.data.title}</h6>
+                  <p class="preface m-0">${res.data.preface}</p>
+                  <hr>
+                  <div class="mb-4">${res.data.content}</div>
+                  <small class="text-muted">Issued on ${moment(res.data.date_publish).format('DD MMMM YYYY h:mm:ss a')} by ${res.data.author.name}</small>
+               </div>
+            `);
+            $('#previewMore').modal();
+         })
+         .catch(err => {
+            console.log(err)
+         })
+   });
 });
